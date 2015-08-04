751f4be87b2547edc7d090d7a7a2a1231b22f17f
Peter Dai <pdai@irdeto.com>
2015-08-04 18:07:40 +0800

    avoid multiple xwiki by change the download path to /

--- versionPicker.py
+++ versionPicker.py
@@ -1,13 +1,13 @@
 import re
 import os
-file = open("xwikiDownloadPage.html","r")                       
+file = open("xwikiDownloadPage.html","r")
 
 latest = ""
-for line in file:                                 
+for line in file:
     if re.search('xwiki-enterprise-web-([0-9].)+war',line):
         latest =  line
 
 xwikiPackage = re.search('xwiki-enterprise-web-([0-9].)+war',latest).group(0)
 
-print 'wget "http://download.forge.ow2.org/xwiki/'+ xwikiPackage + '" -P /var/lib/tomcat7/webapps'
-print 'unzip "/var/lib/tomcat7/webapps/' + xwikiPackage + '" -d /var/lib/tomcat7/webapps/xwiki'
+print 'wget "http://download.forge.ow2.org/xwiki/'+ xwikiPackage + '" -P /'
+print 'unzip "/' + xwikiPackage + '" -d /var/lib/tomcat7/webapps/xwiki'
